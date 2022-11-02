75571b94105a13e1ff8b07a4254fab5bf229ba10
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2022-11-02 15:28:22 +0000

    level300-done

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 //You can edit ALL of the code here
 
+  let selectItems= document.getElementById("selectEpisode");
+  let span= document.getElementById("search-result"); //span for dispaly
+  
+  
+// step one
 function setup() {
   const allEpisodes = getAllEpisodes();
+  makeSelectBoxForEpisode( allEpisodes );
   makePageForEpisodes(allEpisodes); 
 }
- 
 
+
+//step two
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   let underorderedListEl = document.createElement("ul");
@@ -17,7 +24,7 @@ function makePageForEpisodes(episodeList) {
       headerDiv.appendChild(spanTitle);
       headerDiv.append("-")
       let spanSeries= document.createElement("span");
-      spanSeries.innerText = `S0${episode.season}E0${episode.number}`;
+      spanSeries.innerText = `S0${episode.season}E${episode.number > 9 ? episode.number:"0"+ episode.number}`;
       headerDiv.appendChild(spanSeries);
       listItemEl.appendChild(headerDiv);
 
@@ -34,12 +41,11 @@ function makePageForEpisodes(episodeList) {
 rootElem.innerHTML = '';
 rootElem.appendChild(underorderedListEl);
 }
-
+//step three- give search feature
 let input = document.getElementById("search");
 input.addEventListener("keyup",(event)=>{
   const allEpisodes = getAllEpisodes(); // data comes form it 
   let keyword = event.target.value.toLowerCase(); // User type in input 
-
   let result = [];
   allEpisodes.forEach( x =>{ 
     let summary = x.summary.toLowerCase();
@@ -47,14 +53,32 @@ input.addEventListener("keyup",(event)=>{
       if( summary.indexOf( keyword ) > -1 || name.indexOf(keyword) > -1 ){
          result.push( x );
       } 
-  })  
-  let resultCount    = result.length;
-  let episodestCount = allEpisodes.length;
-  let p= document.getElementById("display-me").style.display = "none";
-
+ 
+  })   
+  span.innerText= `Displaying ${result.length}/${allEpisodes.length} Episodes`;
   
   makePageForEpisodes( result );
 })
+// step four- episode list in select element
+function makeSelectBoxForEpisode( episodeList ){
+  episodeList.forEach(list=>{
+    let inputlist= document.createElement("option");
+    inputlist.setAttribute("value", list.id);
+    inputlist.innerText = `S0${list.season}E${list.number > 9 ? list.number:"0"+ list.number} - ${list.name}`
+    selectItems.append(inputlist);
+  })
+}
+// step five- set roll up and down list of episodes  
+selectItems.addEventListener("change",()=>{
+  const allEpisodes = getAllEpisodes(); 
+  allEpisodes.forEach( nameList =>{
+    if( nameList.id == event.target.value ){
+      makePageForEpisodes( [nameList] );
+
+    }
+  })
+})
+
 
 window.onload = setup;
 
